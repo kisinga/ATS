e7fecda76301425c23eb9a7e84d6d205454a3c11
kisinga <kamana@kisinga.family>
2020-12-30 14:16:56 +0300

    added icons and manifest

--- frontend/src/app/@core/core.module.ts
+++ frontend/src/app/@core/core.module.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbAuthJWTToken, NbAuthModule, NbDummyAuthStrategy, NbPasswordAuthStrategy } from '@nebular/auth';
+import { NbAuthModule, NbDummyAuthStrategy } from '@nebular/auth';
 import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
 import { of as observableOf } from 'rxjs';
 
@@ -31,46 +31,19 @@ export const NB_CORE_PROVIDERS = [
   ...NbAuthModule.forRoot({
 
     strategies: [
-      NbPasswordAuthStrategy.setup({
+      NbDummyAuthStrategy.setup({
         name: 'email',
-        token: {
-          class: NbAuthJWTToken,
-        },
-        baseEndpoint: '',
-        login: {
-          // ...
-          endpoint: '/api/auth/login',
-        },
-        logout: {
-          endpoint: '/api/auth/sign-out',
-        },
-
+        delay: 3000,
       }),
     ],
     forms: {
-      redirectDelay: 500, // delay before redirect after a successful login, while success message is shown to the user
-      strategy: 'email',  // strategy id key.
-      rememberMe: false,   // whether to show or not the `rememberMe` checkbox
-      showMessages: {     // show/not show success/error messages
-        success: true,
-        error: true,
+      login: {
+        socialLinks: socialLinks,
       },
-      socialLinks: socialLinks, // social links at the bottom of a page
-      logout: {
-        redirectDelay: 500,
-        strategy: 'email',
+      register: {
+        socialLinks: socialLinks,
       },
-      validation: {
-        password: {
-          required: true,
-          minLength: 5,
-          maxLength: 50,
-        },
-        email: {
-          required: true,
-        },
-      },
-    }
+    },
   }).providers,
 
   NbSecurityModule.forRoot({
