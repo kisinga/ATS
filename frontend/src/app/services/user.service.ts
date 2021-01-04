eec139083a28ae1127d7c86d1fcbe8839d135a49
kisinga <kamana@kisinga.family>
2021-01-04 18:25:03 +0300

    fixed lint errors

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,26 +2,23 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import firebase from 'firebase/app';
-import { ReplaySubject } from "rxjs";
-
+import { ReplaySubject } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   user = new ReplaySubject<firebase.UserInfo>(1);
 
-  constructor(private auth: AngularFireAuth,
-    router: Router
-  ) {
-    auth.authState.subscribe(state => {
+  constructor(private auth: AngularFireAuth, router: Router) {
+    auth.authState.subscribe((state) => {
       if (state) {
-        if (router.routerState.snapshot.url !== "/") {
-          router.navigate(["/"]);
+        if (router.routerState.snapshot.url !== '/') {
+          router.navigate(['/']);
         }
-        this.user.next(state)
+        this.user.next(state);
       } else {
-        if (router.routerState.snapshot.url !== "/login") {
-          router.navigate(["/login"]);
+        if (router.routerState.snapshot.url !== '/login') {
+          router.navigate(['/login']);
         }
       }
     });
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -6,12 +6,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCNfI8MiFsvJtQEcJPg3tNPSS8SvF5rDfA",
-    authDomain: "ats-ke.firebaseapp.com",
-    projectId: "ats-ke",
-    storageBucket: "ats-ke.appspot.com",
-    messagingSenderId: "224661080187",
-    appId: "1:224661080187:web:a3ed310081739942a87ade",
-    measurementId: "G-PHLQH8T7PP"
+    apiKey: 'AIzaSyCNfI8MiFsvJtQEcJPg3tNPSS8SvF5rDfA',
+    authDomain: 'ats-ke.firebaseapp.com',
+    projectId: 'ats-ke',
+    storageBucket: 'ts-ke.appspot.com',
+    messagingSenderId: '224661080187',
+    appId: '1:224661080187:web:a3ed310081739942a87ade',
+    measurementId: 'G-PHLQH8T7PP'
   }
 };
