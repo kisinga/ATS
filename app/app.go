c85b6689c4f2fe41d404f1e2c8ced9bfc79d3da2
kisinga <kamana@kisinga.family>
2021-01-06 16:21:51 +0300

    configured auth middleware route

--- app/app.go
+++ app/app.go
@@ -2,11 +2,13 @@ package app
 
 import (
 	"net/http"
+	"time"
 
 	firebase "firebase.google.com/go"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/kisinga/ATS/app/auth"
 	"github.com/kisinga/ATS/app/gql/generated"
 	"github.com/kisinga/ATS/app/gql/resolvers"
@@ -26,7 +28,11 @@ func NewApp(db *storage.Database, firebase *firebase.App, port string, prod bool
 		Debug:            true,
 	}).Handler)
 
-	router.Use(auth.Middleware(firebase))
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Timeout(5 * time.Second))
 
 	domain := registry.NewDomain(db)
 
@@ -34,9 +40,12 @@ func NewApp(db *storage.Database, firebase *firebase.App, port string, prod bool
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: r}))
 	router.Handle("/playground", playground.Handler("GraphQL", "/api"))
-	router.Handle("/api", srv)
-	router.Handle("/sessionInit", auth.SessionInit(firebase))
-	router.Handle("/sessionTerm", auth.SessionTerm())
+	router.Route("/api", func(r chi.Router) {
+		router.Use(auth.Middleware(firebase))
+		router.Handle("/api", srv)
+	})
+	router.Post("/sessionInit", auth.SessionInit(firebase))
+	router.Get("/sessionTerm", auth.SessionTerm())
 
 	return http.ListenAndServe(port, router)
 }
