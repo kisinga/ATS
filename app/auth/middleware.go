f8dd399177034cd94c652c3bbc942b7961a26b70
kisinga <kamana@kisinga.family>
2021-01-08 13:20:09 +0300

    💉injected 💉 user data into context

--- app/auth/middleware.go
+++ app/auth/middleware.go
@@ -2,11 +2,22 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 
 	firebase "firebase.google.com/go"
+	"github.com/kisinga/ATS/app/models"
 )
+
+// A private key for context that only this package can access. This is important
+// to prevent collisions between different context uses
+var userCtxKey = &contextKey{"user"}
+
+type contextKey struct {
+	name string
+}
 
 func Middleware(firebase *firebase.App) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -30,13 +41,27 @@ func Middleware(firebase *firebase.App) func(http.Handler) http.Handler {
 			// Verify the session cookie. In this case an additional check is added to detect
 			// if the user's Firebase session was revoked, user deleted/disabled, etc.
 			client, err := firebase.Auth(context.Background())
-			_, err = client.VerifySessionCookieAndCheckRevoked(r.Context(), token)
+			firebaseToken, err := client.VerifySessionCookieAndCheckRevoked(r.Context(), token)
 			if err != nil {
 				// Session cookie is invalid. Force user to login.
 				http.Error(w, "Invalid token", http.StatusForbidden)
 				return
 			}
+			user := models.User{}
+			dbByte, err := json.Marshal(firebaseToken.Claims)
+			err = json.Unmarshal(dbByte, &user)
+			if err != nil {
+				// Session cookie is invalid. Force user to login.
+				http.Error(w, "Error Marshalling userdata into json", http.StatusInternalServerError)
+				return
+			}
 			// Token is authenticated, pass it through
+			fmt.Println(user)
+			// put it in context
+			ctx := context.WithValue(r.Context(), userCtxKey, user)
+
+			// and call the next with our new context
+			r = r.WithContext(ctx)
 			next.ServeHTTP(w, r)
 		})
 	}
